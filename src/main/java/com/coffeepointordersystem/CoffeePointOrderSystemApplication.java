52bb85fc2aaffef2ba5aad76b0a8a486b07421de
kolyn092 <kolyn092@gmail.com>
2026-07-14 16:10:33 +0900

    Initial commit

---
+++ src/main/java/com/coffeepointordersystem/CoffeePointOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.coffeepointordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeePointOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeePointOrderSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coffeepointordersystem/CoffeePointOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeepointordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeePointOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
