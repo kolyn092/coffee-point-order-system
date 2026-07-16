8b4167e7307cbd253b5c77026c895ec2ee3331dd
kolyn092 <kolyn092@gmail.com>
2026-07-16 13:01:41 +0900

    test: 주문 결제 정합성 검증 추가 (#8)

--- src/test/java/com/coffeepointordersystem/domain/point/entity/PointAccountTest.java
+++ src/test/java/com/coffeepointordersystem/domain/point/entity/PointAccountTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.coffeepointordersystem.domain.point.exception.PointBalanceLimitExceededException;
+import com.coffeepointordersystem.domain.point.exception.InsufficientPointBalanceException;
 import org.junit.jupiter.api.Test;
 
 class PointAccountTest {
@@ -15,6 +16,15 @@ class PointAccountTest {
 		assertThatThrownBy(() -> pointAccount.charge(1L))
 				.isInstanceOf(PointBalanceLimitExceededException.class);
 		assertThat(pointAccount.getBalance()).isEqualTo(Long.MAX_VALUE);
+	}
+
+	@Test
+	void use_throwsExceptionAndPreservesBalanceWhenBalanceIsInsufficient() {
+		PointAccount pointAccount = new PointAccount("point-unit-user", 4_499L);
+
+		assertThatThrownBy(() -> pointAccount.use(4_500L))
+				.isInstanceOf(InsufficientPointBalanceException.class);
+		assertThat(pointAccount.getBalance()).isEqualTo(4_499L);
 	}
 
 }
